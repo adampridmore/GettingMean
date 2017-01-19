0055effc331a5aab2a9e895b24b746ca4973938c
Adam Pridmore <adam_pridmore@hotmail.com>
2017-01-19 20:06:46 +0000

    - Added a location list.

--- WeatherScopesAngular/App/app.module.js
+++ WeatherScopesAngular/App/app.module.js
@@ -4,7 +4,21 @@ var myController = function($scope) {
     $scope.myInput = "world!";
 };
 
+var locationList = function($scope) {
+    $scope.data = {
+        locations: [
+            {
+                name: 'Burger King',
+                address: '125 High Street, Reading, RG6 1PS'
+            }, {
+                name: 'Costy',
+                address: '7 Stanborough Road, Plymouth, PL9 8SP'
+            }
+        ]
+    }
+}
+
 angular
     .module('myApp')
-    .controller('myController', myController);
-
+    .controller('myController', myController)
+    .controller('locationList', locationList);
