4c75fbd0e20a06f2e979925fa6d94d94ba55a16e
Adam Pridmore <adam_pridmore@hotmail.com>
2017-01-19 20:37:36 +0000

    - Added simple REST Api method to return location.  - Updated Newtonsoft.Json package version.  -

--- WeatherScopesAngular/App_Start/WebApiConfig.cs
+++ WeatherScopesAngular/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WeatherScopesAngular
@@ -17,8 +15,11 @@ namespace WeatherScopesAngular
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new {id = RouteParameter.Optional}
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ WeatherScopesAngular/Controllers/AppDataController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WeatherScopesAngular.Controllers
+{
+    public class AppDataController : ApiController
+    {
+        [HttpGet]
+        public List<Location> Get()
+        {
+            return new List<Location>
+            {
+                new Location {Name = "My Location 1", Address = "My location 1 address"},
+                new Location {Name = "My Location 2", Address = "My location 2 address"}
+            };
+        }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
